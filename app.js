8378598359e45cfbc2a8c98d68117ec6cc0782fc
rexmirak <karim.ahmed766@yahoo.com>
2022-06-19 21:10:17 +0200

    added new funcs

--- app.js
+++ app.js
@@ -28,3 +28,172 @@ function calculateGPA(courseList) {
   }
   return totalPts / totalHours;
 }
+
+//look up functions
+function getNumericGPA(charGrade) {
+  ///////////////////////////////////////
+  // takes a char of course grade and  //////
+  // returns the corresponding numeric GPA //
+  ///////////////////////////////////////////
+  let grade = 0;
+  charGrade = charGrade.toLowerCase();
+  switch (charGrade) {
+    case "A+":
+      grade = 0.7;
+      break;
+    case "A":
+      grade = 1.0;
+      break;
+    case "A-":
+      grade = 1.3;
+      break;
+    case "B+":
+      grade = 1.7;
+      break;
+    case "B":
+      grade = 2.0;
+      break;
+    case "B-":
+      grade = 2.3;
+      break;
+    case "C+":
+      grade = 2.7;
+      break;
+    case "C":
+      grade = 3.0;
+      break;
+    case "C-":
+      grade = 3.3;
+      break;
+    case "D+":
+      grade = 3.7;
+      break;
+    case "D":
+      grade = 4.0;
+      break;
+    case "F":
+      grade = 5.0;
+      break;
+    default:
+      console.log("WRONG GRADE ENTERED");
+  }
+
+  return grade;
+}
+
+function getUpperBoundPercentage(charGrade) {
+  ///////////////////////////////////////
+  // takes a char of course grade and  //
+  // returns the corresponding //////////
+  // upper bound percentage    //
+  ///////////////////////////////
+  let upperPercentage = 0;
+  charGrade = charGrade.toLowerCase();
+  switch (charGrade) {
+    case "A+":
+      upperPercentage = 100;
+      break;
+    case "A":
+      upperPercentage = 93.9;
+      break;
+    case "A-":
+      upperPercentage = 89.9;
+      break;
+    case "B+":
+      upperPercentage = 85.9;
+      break;
+    case "B":
+      upperPercentage = 81.9;
+      break;
+    case "B-":
+      upperPercentage = 77.9;
+      break;
+    case "C+":
+      upperPercentage = 73.9;
+      break;
+    case "C":
+      upperPercentage = 69.9;
+      break;
+    case "C-":
+      upperPercentage = 64.9;
+      break;
+    case "D+":
+      upperPercentage = 59.9;
+      break;
+    case "D":
+      upperPercentage = 54.9;
+      break;
+    case "F":
+      upperPercentage = 49.9;
+      break;
+    default:
+      console.log("WRONG GRADE ENTERED");
+  }
+
+  return upperPercentage;
+}
+
+function getLowerBoundPercentage(charGrade) {
+  ///////////////////////////////////////
+  // takes a char of course grade and  //
+  // returns the corresponding //////////
+  // upper bound percentage    //
+  ///////////////////////////////
+  let lowerPercentage = 0;
+  charGrade = charGrade.toLowerCase();
+  switch (charGrade) {
+    case "A+":
+      lowerPercentage = 94;
+      break;
+    case "A":
+      lowerPercentage = 90;
+      break;
+    case "A-":
+      lowerPercentage = 86;
+      break;
+    case "B+":
+      lowerPercentage = 82;
+      break;
+    case "B":
+      lowerPercentage = 78;
+      break;
+    case "B-":
+      lowerPercentage = 74;
+      break;
+    case "C+":
+      lowerPercentage = 70;
+      break;
+    case "C":
+      lowerPercentage = 65;
+      break;
+    case "C-":
+      lowerPercentage = 60;
+      break;
+    case "D+":
+      lowerPercentage = 55;
+      break;
+    case "D":
+      lowerPercentage = 50;
+      break;
+    case "F":
+      lowerPercentage = 0;
+      break;
+    default:
+      console.log("WRONG GRADE ENTERED");
+  }
+
+  return lowerPercentage;
+}
+
+function getCharGPA(numGPA) {
+  let charGPA;
+  if (numGPA <= 0.99) charGPA = "A+";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+  if (numGPA >= 0.99 && numGPA <= 1.29) charGPA = "A";
+}
