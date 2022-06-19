b81e0f97d85ed69e72176f9834ae10126d479662
rexmirak <karim.ahmed766@yahoo.com>
2022-06-19 14:11:15 +0200

    nina project

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 let x = "karim";
+let y = "Nina";
+let hayaty =
+  "Cpnstant misery and pain inside, please end it, please end me and end wogoody 2bl manheeh b nafsy.";
+let alby =
+  "ninz's life and existence makes it not rotten and void, please stay cus you are literally, literally, fucking literally the only one keeping me sane <3 ";
